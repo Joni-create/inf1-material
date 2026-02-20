8e88a0f339d85e65b151122a8a81b35f71b975bc
Joni-create <s251966@student.dhbw-mannheim.de>
2026-02-20 14:41:08 +0100

    labor

--- probeklausuren/praxis/probeklausur1/aufgaben/aufgabe4/aufgabe4_test.go
+++ probeklausuren/praxis/probeklausur1/aufgaben/aufgabe4/aufgabe4_test.go
@@ -6,13 +6,16 @@ func ExampleMaxElements() {
 	l1 := []int{0, 2, 0, 2, 0, 2, 0, 2}
 	l2 := []int{2, 0, 2, 0, 2, 0, 2, 0}
 	l3 := []int{1, 1, 1, 1, 1}
+	l5 := []int{}
 
 	fmt.Println(MaxElements(l1, l2))
 	fmt.Println(MaxElements(l2, l1))
 	fmt.Println(MaxElements(l1, l3))
+	fmt.Println(MaxElements(l5, l5))
 
 	// Output:
 	// [2 2 2 2 2 2 2 2]
 	// [2 2 2 2 2 2 2 2]
 	// [1 2 1 2 1 2 0 2]
+	// []
 }
--- probeklausuren/praxis/probeklausur2/aufgaben/aufgabe4/aufgabe4.go
+++ probeklausuren/praxis/probeklausur2/aufgaben/aufgabe4/aufgabe4.go
@@ -13,7 +13,24 @@ package aufgabe4
 // hinteren Werte ihr letztes Element verwendet werden.
 // Für leere Listen soll für die Berechnung ggf. 0 verwendet werden.
 func ElementSums(l1, l2 []int) []int {
-	result := []int{}
+	lnew := []int{}
 	// TODO
-	return result
+	iMax1 := len(l1)
+	l1Max := l1[iMax1-1]
+	iMax2 := len(l2)
+	l2Max := l2[iMax2-1]
+	for i := 0; i < len(l1) || i < len(l2); i++ {
+
+		if i < len(l1) && i < len(l2) {
+			lnew = append(lnew, l1[i]+l2[i])
+		}
+
+		if i >= len(l2) && i < len(l1) {
+			lnew = append(lnew, l1[i]+l2Max)
+		}
+		if i >= len(l1) && i < len(l2) {
+			lnew = append(lnew, l2[i]+l1Max)
+		}
+	}
+	return lnew
 }
